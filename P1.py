97cdb230c7e5e007829abd5006fda6e335c77b5f
andreghenssler2 <113136868+andreghenssler2@users.noreply.github.com>
2025-05-13 12:08:51 -0300

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,13 @@
+l1 = [1, 2, 4]
+l2 = [1, 3, 4]
+print("Entrda L1: ",l1)
+
+print("Entrda L2: ",l2)
+# Junta as duas listas
+resultado = l1 + l2
+
+# Ordena a lista
+resultado.sort()
+
+# Imprime o resultado
+print("Saida ",resultado)  # Saída esperada: [1, 1, 2, 3, 4, 4]
